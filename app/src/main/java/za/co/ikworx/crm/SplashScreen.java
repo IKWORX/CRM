90a2275019e91d8ca93c9511151075470954e63b
Sbuda95 <Sbuda95@gmail.com>
2018-05-04 14:54:54 +0200

    spash

---
+++ app/src/main/java/za/co/ikworx/crm/SplashScreen.java
@@ -0,0 +1,34 @@
+package za.co.ikworx.crm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+       // this.requestWindowFeature(this.FEATURE_NO_TITLE);
+
+        Thread myThread = new Thread(){
+            @Override
+
+            public void run(){
+
+                try{
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+
+        };
+            myThread.start();
+
+    }
+}
