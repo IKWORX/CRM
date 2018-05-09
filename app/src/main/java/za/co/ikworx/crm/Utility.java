e8c4db097e6dedffc0095ae0a8009ac48bc57029
Godfrey <gntimba@gmail.com>
2018-05-09 15:26:14 +0200

    newLogin

--- app/src/main/java/za/co/ikworx/crm/Utility.java
+++ app/src/main/java/za/co/ikworx/crm/Utility.java
@@ -2,8 +2,31 @@ package za.co.ikworx.crm;
 
 public class Utility {
 
-    private String IP;
+    public String getIP() {
+        return IP;
+    }
+
+    private String IP="http://192.168.176.33/androidDB/";
     private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+
 
 
 }
