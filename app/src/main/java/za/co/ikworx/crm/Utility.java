f9bc664f41472f0aee6ac7f87c60a88aabd2f275
Sbuda95 <Sbuda95@gmail.com>
2018-05-09 14:00:20 +0200

    register page with php file that needs editting

--- app/src/main/java/za/co/ikworx/crm/Utility.java
+++ app/src/main/java/za/co/ikworx/crm/Utility.java
@@ -2,8 +2,19 @@ package za.co.ikworx.crm;
 
 public class Utility {
 
-    private String IP;
+    private String IP ="http://192.168.176.33/adndroidDB/";
     private String username;
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getIP() {
+
+        return IP;
+    }
 }
